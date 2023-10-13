b0520d9a259fffc2f4a058909aa1ff383afa98df
mende <mendezcamila935@gmail.com>
2023-10-12 21:56:24 -0500

    Se integra el formulario de registro funcional

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy {
+  formRegister: FormGroup = new FormGroup({});
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formRegister = this.fb.group({
+      fullName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  ngOnDestroy(): void {
+    console.log('Adios');
+  }
+
+  onSubmit(): void {
+    if (this.formRegister.invalid) {
+      return;
+    }
+
+    const { fullName, email, password } = this.formRegister.value;
+
+    console.log({ fullName, email, password });
+  }
 }
